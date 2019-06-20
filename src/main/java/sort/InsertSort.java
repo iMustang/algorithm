f6048d16acd6a503dff2af3736e3a6b48a0c91e8
xMustang <xmustang@xMustangs-MacBook-Pro.local>
2019-06-21 03:16:27 +0800

    希尔排序

--- src/main/java/sort/InsertSort.java
+++ src/main/java/sort/InsertSort.java
@@ -5,17 +5,18 @@ import java.util.Arrays;
 /**
  * 直接插入排序
  * 与打扑克时整理牌原理相同：将新的牌与已有的牌从后向前一张张比较，插入合适位置
+ * 因为前面序列是有序的，如1,3,4,6，再插入5时，5<6，需要继续往前比较，5>4，不需要再比较，要break
  */
 public class InsertSort {
-    static void sort(int[] a) {
-        int n = a.length;
-        int temp = 0;
-        for (int i = 1; i < n; i++) {
+    static void insertSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
             for (int j = i; j > 0; j--) {
                 if (a[j] < a[j - 1]) {
-                    temp = a[j];
+                    int temp = a[j];
                     a[j] = a[j - 1];
                     a[j - 1] = temp;
+                } else {
+                    break;
                 }
             }
         }
@@ -23,7 +24,7 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 1, 6, 9, 0, 8, 5, 3};
-        sort(arr);
+        insertSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
--- src/main/java/sort/ShellSort.java
+++ src/main/java/sort/ShellSort.java
@@ -1,7 +1,33 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * 希尔排序
+ * 希尔排序在数组中采用跳跃式分组的策略，通过某个增量将数组元素划分为若干组，然后分组进行插入排序，随后逐步缩小增量，继续按组进行插入排序操作，直至增量为1。
  */
 public class ShellSort {
+    static void shellSort(int[] a) {
+        int n = a.length;
+        for (int gap = n / 2; gap > 0; gap = gap / 2) {
+            // [4,3,2,1]，第一次gap=2，分成两组[4,2]、[3,1]，此时a[gap]处为2，是第一个分组
+            for (int i = gap; i < a.length; i++) {
+                for (int j = i; j > gap - 1; j = j - gap) {
+                    if (a[j] < a[j - gap]) {
+                        int tmp = a[j];
+                        a[j] = a[j - gap];
+                        a[j - gap] = tmp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 8, 0, 5, 2, 6};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
