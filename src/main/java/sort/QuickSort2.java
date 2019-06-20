6fe37a1da0c363bfbc7a3275fced6629475a68df
xMustang <xmustang@xMustangs-MacBook-Pro.local>
2019-06-21 00:59:21 +0800

    快速排序指针法

--- src/main/java/sort/QuickSort2.java
+++ src/main/java/sort/QuickSort2.java
@@ -3,33 +3,36 @@ package sort;
 import java.util.Arrays;
 
 /**
- * 快速排序(挖坑法）
- * http://www.sohu.com/a/246785807_684445
- * https://www.jianshu.com/p/5f38dd54b11f
+ * 快速排序（指针交换法）
  */
-public class QuickSort {
+public class QuickSort2 {
     static void quickSort(int[] a, int left, int right) {
         if (left >= right) {
             return;
         }
-        int pivot = division(a, left, right);
-        quickSort(a, left, pivot - 1);
-        quickSort(a, pivot + 1, right);
+        int pivotIndex = division(a, left, right);
+        quickSort(a, left, pivotIndex - 1);
+        quickSort(a, pivotIndex + 1, right);
     }
 
     static int division(int[] a, int left, int right) {
         int pivot = a[left];
+        int pivotIndex = left;
+        int tmp = 0;
         while (left < right) {
             while (left < right && a[right] >= pivot) {
                 right--;
             }
-            a[left] = a[right];
             while (left < right && a[left] <= pivot) {
                 left++;
             }
-            a[right] = a[left];
+            tmp = a[left];
+            a[left] = a[right];
+            a[right] = tmp;
         }
+        tmp = a[left];
         a[left] = pivot;
+        a[pivotIndex] = tmp;
         return left;
     }
 
