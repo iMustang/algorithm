887d1ac4fde4c26774cada09dae84eccf09ae5de
xMustang <xMustang@foxmail.com>
2019-06-21 13:14:25 +0800

    参考文档

--- src/main/java/sort/CockTailSort.java
+++ src/main/java/sort/CockTailSort.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 /**
  * 鸡尾酒排序
- * 冒泡排序的优化版
+ * https://mp.weixin.qq.com/s?__biz=MzIxMjE5MTE1Nw==&mid=2653194919&idx=2&sn=f473bc9e0da124a303312a11902e2f52&chksm=8c99f87dbbee716b531df3fcf5882998f28794aad5609f225883d6c2dc71ba51b8a5126b32be&mpshare=1&scene=23&srcid=06219uT9FjnJSp3HoEZ4tyA9#rd
+ * 冒泡排序的另一种优化
  */
 public class CockTailSort {
     static void cockTailSort(int[] a) {
